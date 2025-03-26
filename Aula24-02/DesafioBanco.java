590d6d2d301ebb4ce35cbbdf6a69190e97bbd53d
lucaswgoncalves616 <lucas.wendell.98@gmail.com>
2025-03-26 10:39:02 -0300

    Aula do dia 24

--- Aula24-02/DesafioBanco.java
+++ Aula24-02/DesafioBanco.java
@@ -1,9 +1,35 @@
 package aula24_02;
 
+import java.util.Scanner;
+
 public class DesafioBanco {
 
+	double saldo;
+
+	public DesafioBanco(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public double deposito(double saldoDeposito) {
+		return saldo + saldoDeposito;
+	}
+
+	public double saque(double saldoSaque) {
+		return saldo - saldoSaque;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite o saldo da sua conta: ");
+		double saldo = sc.nextDouble();
+		DesafioBanco cliente = new DesafioBanco((saldo));
+
+
 
 	}
 
