8311f903198cf38919fdfd6ea8ccc784a00696f6
Lucas Wendell <lucas.wendell.98@gmail.com>
2025-03-31 15:06:30 -0300

    Exercicios do dia 31

--- Aula31-03/ArrayExercicio03.java
+++ Aula31-03/ArrayExercicio03.java
@@ -1,10 +1,21 @@
 public class ArrayExercicio03 {
     public static void main(String[] args) {
         int[] numeros = {5,3,9,1,7,8,4,2};
-        int maior, menor = 0;
+        int maior = 0;
+        int menor = 10;
 
-        for(int i = 0; i < numeros.length; i++) {
-
+        for (int numero : numeros) {
+            if (numero > maior) {
+                maior = numero;
+            }
+            if (numero < menor) {
+                menor = numero;
+            }
         }
+
+        System.out.println("O menor numero é: " + menor);
+        System.out.println("O maior numero é: " + maior);
+
+
     }
 }
