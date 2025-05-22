c6ec0936c18ce7ae594c693c6c82606239889b9f
Lucas Wendell <lucas.wendell.98@gmail.com>
2025-05-22 20:51:32 -0300

    Desafio do dia 19

--- Aula - Maio Dia 19/Jogo_de_Pedra_Papel_Tesoura_2/Jokenpo2.java
+++ Aula - Maio Dia 19/Jogo_de_Pedra_Papel_Tesoura_2/Jokenpo2.java
@@ -4,12 +4,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Jokenpo2 {
-    public static void main(String[] args) {
+    public static int recebeJogada() {
         Scanner sc = new Scanner(System.in);
-        Random rand = new Random();
-        int jogada;
-
-        System.out.println("Vamos começar o jogo...");
+        int jogadaPlayer;
         System.out.println("""
                 Selecione uma das opções:
                 1. Pedra
@@ -19,8 +16,79 @@ public class Jokenpo2 {
                 5. Spock
                 """);
         System.out.print("Sua jogada: ");
-        jogada = sc.nextInt();
 
-        System.out.println("Jogada do computador: ");
+        do {
+            jogadaPlayer = sc.nextInt();
+            if (jogadaPlayer >= 1 && jogadaPlayer < 6) {
+                jogadaPlayer = switch (jogadaPlayer) {
+                    case 1 -> 1;
+                    case 2 -> 2;
+                    case 3 -> 3;
+                    case 4 -> 4;
+                    case 5 -> 5;
+                    default -> jogadaPlayer;
+                };
+                break;
+            } else {
+                System.out.print("Opção inválida!\nDigite novamente: ");
+            }
+        } while (true);
+        return jogadaPlayer;
+    }
+
+    public static String escolhaJogada(int recebeJogada) {
+        String jogada = "";
+        switch (recebeJogada) {
+            case 1: jogada = "Pedra";
+                break;
+            case 2: jogada = "Papel";
+                break;
+            case 3: jogada = "Tesoura";
+                break;
+            case 4: jogada = "Lagarto";
+                break;
+            case 5: jogada = "Spock";
+                break;
+        }
+        return jogada;
+    }
+
+    public static String compara(String player, String cpu) {
+        String[][] regraVitoria = {
+                {"Tesoura", "Papel", "Tesoura corta o papel!"},
+                {"Papel", "Pedra", "Papel cobre a pedra!"},
+                {"Pedra", "Lagarto", "Pedra esmaga o lagarto!"},
+                {"Lagarto", "Spock", "Lagarto envenena o spock!"},
+                {"Spock", "Tesoura", "Spock esmaga a tesoura!"},
+                {"Tesoura", "Lagarto", "Tesoura decapita o lagarto!"},
+                {"Lagarto", "Papel", "Lagarto come o papel!"},
+                {"Papel", "Spock", "Papel desmente o spock!"},
+                {"Spock", "Pedra", "Spock destrói a pedra!"},
+                {"Pedra", "Tesoura", "Pedra quebra a tesoura!"},
+        };
+        for (String[] strings : regraVitoria) {
+            if (player.equals(strings[0]) && cpu.equals(strings[1])) {
+                return "\nVoce venceu! " + strings[2];
+            } else if (cpu.equals(strings[0]) && player.equals(strings[1])) {
+                return "\nVoce perdeu! " + strings[2];
+            }
+        }
+        return "Empate!";
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        String jogadaPlayer;
+        String jogadaCpu;
+
+        System.out.println("Vamos começar o jogo...\n");
+
+        jogadaPlayer = escolhaJogada(recebeJogada());
+        System.out.println("Voce escolheu: " + jogadaPlayer + "!");
+
+        jogadaCpu = escolhaJogada(rand.nextInt(1, 6));
+        System.out.println("CPU escolheu: " + jogadaCpu + "!");
+
+        System.out.println(compara(jogadaPlayer, jogadaCpu));
     }
 }
