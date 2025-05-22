0436e8a99902ed09af8c2b064479517d5addd398
lucaswgoncalves616 <lucas.wendell.98@gmail.com>
2025-05-22 10:38:27 -0300

    Desafio do dia 19

--- Aula - Maio Dia 19/Jogo_de_Pedra_Papel_Tesoura_Extendido/Jokepo.java
+++ Aula - Maio Dia 19/Jogo_de_Pedra_Papel_Tesoura_Extendido/Jokepo.java
@@ -2,6 +2,6 @@ package Jogo_de_Pedra_Papel_Tesoura_Extendido;
 
 public class Jokepo {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("agora vai");
     }
 }
